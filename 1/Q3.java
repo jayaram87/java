0b205ece66dc1716fa05452b660745a962197d1d
jayaramraja <jayaramraja1987@gmail.com>
2022-10-24 18:00:33 -0400

    asssignment1

---
+++ 1/Q3.java
@@ -0,0 +1,25 @@
+package q3;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 13;
+		for (int i = 0; i < n; i++) {
+			for (int j =0; j < n; j++) {
+				if (i == 0 || 
+						i == n-1 || 
+						j == 0 || 
+						j == n-1 ||
+						(i+j <= n/2) ||
+						(j-i >= n/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ 1/Q4.java
@@ -0,0 +1,19 @@
+package q4;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 15;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i >= n-2 || i-j >= (n-1)/2 || i+j >= (n + (n-1)/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
